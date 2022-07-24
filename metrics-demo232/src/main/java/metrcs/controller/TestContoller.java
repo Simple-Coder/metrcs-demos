4258569cba65935d4af773fecba8d7b3c185301e
dongie <1412834290@qq.com>
2022-07-24 19:11:56 +0800

    dynamictp 初步使用

--- metrics-demo232/src/main/java/metrcs/controller/TestContoller.java
+++ metrics-demo232/src/main/java/metrcs/controller/TestContoller.java
@@ -1,9 +1,14 @@
 package metrcs.controller;
 
+import com.dtp.core.DtpRegistry;
+import com.dtp.core.thread.DtpExecutor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * Created by xiedong
@@ -15,8 +20,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestContoller {
     //  http:localhost:8989/metric1/a
     // http://127.0.0.1:9998/metrics
+
+    @Resource
+    private ThreadPoolExecutor dtpExecutor1;
+
     @GetMapping("/a")
     public String a() {
+
+//        DtpExecutor dtpExecutor = DtpRegistry.getDtpExecutor("dtpExecutor1");
+//        dtpExecutor.execute(() -> System.out.println("test"));
+
         return Thread.currentThread().getName();
     }
 }
