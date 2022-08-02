150fef248c8fecd7c2fa7b814b47624968a1d9e2
dongxie <dongxie@sohu-inc.com>
2022-08-02 16:50:40 +0800

    //注释无用注解

--- metrics-demo158/src/main/java/com/metrics/controller/TestContoller.java
+++ metrics-demo158/src/main/java/com/metrics/controller/TestContoller.java
@@ -1,6 +1,6 @@
 package com.metrics.controller;
 
-import io.prometheus.client.spring.web.PrometheusTimeMethod;
+//import io.prometheus.client.spring.web.PrometheusTimeMethod;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
