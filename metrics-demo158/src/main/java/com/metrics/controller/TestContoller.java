a0a553f52b58dca1f1a5ca70fad80ea2da3d0eae
dongie <1412834290@qq.com>
2022-08-03 22:47:38 +0800

    COUNTER 初步使用

--- metrics-demo158/src/main/java/com/metrics/controller/TestContoller.java
+++ metrics-demo158/src/main/java/com/metrics/controller/TestContoller.java
@@ -1,7 +1,11 @@
 package com.metrics.controller;
 
 //import io.prometheus.client.spring.web.PrometheusTimeMethod;
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Metrics;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,10 +18,16 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequestMapping("/metric1")
 public class TestContoller {
+    static final Counter userCounter = Metrics.counter("user.counter.total", "services", "demo");
+
+    @Autowired
+    private PrometheusMeterRegistry meterRegistry;
     //  http:localhost:8989/metric1/a
     @GetMapping("/a")
 //    @PrometheusTimeMethod(name = "metric1_a", help = "metric1_a_help")
     public String a() {
+//        Metrics.gauge()
+        userCounter.increment();
         return Thread.currentThread().getName();
     }
 }
