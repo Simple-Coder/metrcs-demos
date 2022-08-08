fd18d36a2e2014030c9ceb81010a0aa0dc15996b
dongxie <dongxie@sohu-inc.com>
2022-08-08 17:59:44 +0800

    eurekaserver ok 1.5.x

---
+++ metrics-eureka-server/src/main/java/com/metrics/eureka/EurekaServer.java
@@ -0,0 +1,16 @@
+package com.metrics.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by dongxie on 2022/8/8.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServer {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer.class, args);
+    }
+}
